60d7deb170156537af4e22bfbca92bf348a8ecde
Szynek <m.szyndler01@gmail.com>
2024-12-10 22:54:16 +0000

    Added More to the Notes via Logseq 2024-12-10T22:54:16.140Z

---
+++ logseq/bak/PostCode_IO_Application_Client/app/2024-12-10T14_57_42.210Z.Desktop.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const axios = require('axios');
+
+const myPostCode = process.env.myPostCode;
+const myOutCode = process.env.myOutCode;
+
+const apiUrl = `https://api.postcodes.io/postcodes/${myPostCode.toLowerCase()}`;
+const outCodeUrl = `https://api.postcodes.io/outcodes/${myOutCode.toLowerCase()}`;
+
+//Request Config
+var requestConfigApi = {
+    method:'get',
+    url:apiUrl,
+    headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+}
+
+var requestConfigOutCode = {
+    method:'get',
+    url:outCodeUrl,
+    headers:{
+        'Accept': 'application/json',
+        'Content-Type':  'application/json'
+    }
+}
+//Create create an instance
+const requestInstance = axios.create();
+const outCodeInstance = axios.create();
+
+//Use instance to create a request
+const postCodeDataRequest = requestInstance.request(requestConfigApi);
+const outCodeDataRequest = requestInstance.request(requestConfigOutCode);
+
+//Handle Response
+postCodeDataRequest
+.then(function(response){
+    console.log("Response Content (JSON)");
+    console.log(response.data);
+})
+.catch(function(error){
+    console.log("Error occured");
+    console.log(error.message);
+})
+
+outCodeDataRequest.then(function(response){
+    console.log(response.data);
+})
---
+++ logseq/bak/PostCode_IO_Application_Client/app/2024-12-10T15_04_13.792Z.Desktop.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const axios = require('axios');
+
+const myPostCode = process.env.myPostCode;
+const arthurPostCode = process.env.arthurPostCode;
+const myOutCode = process.env.myOutCode;
+
+const apiUrl = `https://api.postcodes.io/postcodes/${myPostCode.toLowerCase()}`;
+const outCodeUrl = `https://api.postcodes.io/outcodes/${myOutCode.toLowerCase()}`;
+const apiPostcodesUrl = `https://api.postcodes.io/postcodes`
+
+//Request Config
+var requestConfigApi = {
+    method:'get',
+    url:apiUrl,
+    headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+}
+
+var requestConfigPostcodes = {
+    method:'post',
+    url:apiPostcodesUrl,
+    headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: {[`${myPostCode}`, `${arthurPostCode}`]}
+}
+
+var requestConfigOutCode = {
+    method:'get',
+    url:outCodeUrl,
+    headers:{
+        'Accept': 'application/json',
+        'Content-Type':  'application/json'
+    }
+}
+//Create create an instance
+const requestInstance = axios.create();
+
+//Use instance to create a request
+const postCodeDataRequest = requestInstance.request(requestConfigApi);
+const outCodeDataRequest = requestInstance.request(requestConfigOutCode);
+const poscodeGetAll = requestInstance.request(requestConfigPostcodes);
+
+//Handle Response
+postCodeDataRequest
+.then(function(response){
+    console.log("Response Content (JSON)");
+    console.log(response.data);
+})
+.catch(function(error){
+    console.log("Error occured");
+    console.log(error.message);
+})
+
+outCodeDataRequest.then(function(response){
+    console.log("Outcode");
+    console.log(response.data);
+})
+
+poscodeGetAll.then(function(response){
+    console.log("Postcodes List")
+    console.log(response.data);
+})
